31dbf7ac614d89821d903f2995dd42ad224dbf13
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2018-09-25 22:31:11 -0700

    Modified piston's example Hello World for new main
    Removed terminal things because Windows likes to be non-standard, so writing a cross-platform app in Rust that needs specific code and versions of crates for Windows vs Linux/Mac to then make a terminal-only app would be a bit rediculous

---
+++ src/engine/render.rs
@@ -0,0 +1,8 @@
+// Render wrapper for Piston
+// Author: Bryce "BtheDestroyer" Dixon
+// Created: 9/25/2018
+// Modified: 9/25/2018
+
+struct render {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,77 @@
-extern crate termion;
+extern crate piston_window;
+extern crate find_folder;
 
-use termion::color;
-
-fn prep_terminal() {
-    
-}
-
-fn show_title() {
-    let color_main = color::LightBlue;
-    let color_leaves = color::LightGreen;
-    let color_trunk = color::LightYellow;
-    println!("{}{}{}{}{}{}", color::Fg(color_main), "                           ", color::Fg(color_leaves), "               ", color::Fg(color_main), "                                                              _____   ");
-    println!("{}{}{}{}{}{}", color::Fg(color_main), "        ___________        ", color::Fg(color_leaves), "  ____________ ", color::Fg(color_main), "___________          _____                _____          _____\\    \\  ");
-    println!("{}{}{}{}{}{}", color::Fg(color_main), "       /           \\       ", color::Fg(color_leaves), " /            \\", color::Fg(color_main), "\\          \\       /      |_         _____\\    \\_       /    / |    | ");
-    println!("{}{}{}{}{}{}{}{}{}{}", color::Fg(color_main), "      /    _   _    \\      ", color::Fg(color_leaves), "|\\___/", color::Fg(color_trunk), "\\  \\", color::Fg(color_leaves), "\\___/|", color::Fg(color_main), "\\    /\\    \\     /         \\       /     /|     |     /    /  /___/| ");
-    println!("{}{}{}{}{}{}{}{}{}{}", color::Fg(color_main), "     /    //   \\\\    \\     ", color::Fg(color_leaves), " \\|____", color::Fg(color_trunk), "\\  \\", color::Fg(color_leaves), "___|/", color::Fg(color_main), " |   \\_\\    |   |     /\\    \\     /     / /____/|    |    |__ |___|/ ");
-    println!("{}{}{}{}{}{}", color::Fg(color_main), "    /    //     \\\\    \\    ", color::Fg(color_trunk), "       |  |    ", color::Fg(color_main), "  |      ___/    |    |  |    \\   |     | |_____|/    |       \\       ");
-    println!("{}{}{}{}{}{}{}{}{}{}", color::Fg(color_main), "   /     \\\\_____//     \\   ", color::Fg(color_leaves), "  __", color::Fg(color_trunk), "  /   /", color::Fg(color_leaves), " __ ", color::Fg(color_main), "  |      \\  ____ |     \\/      \\  |     | |_________  |     __/ __    ");
-    println!("{}{}{}{}{}{}{}{}{}{}", color::Fg(color_main), "  /       \\ ___ /       \\  ", color::Fg(color_leaves), " /  \\", color::Fg(color_trunk), "/   /", color::Fg(color_leaves), "_/  |", color::Fg(color_main), " /     /\\ \\/    \\|\\      /\\     \\ |\\     \\|\\        \\ |\\    \\  /  \\   ");
-    println!("{}{}{}{}{}{}", color::Fg(color_main), " /________/|   |\\________\\ ", color::Fg(color_leaves), "|____________/|", color::Fg(color_main), "/_____/ |\\______|| \\_____\\ \\_____\\| \\_____\\|    |\\__/|| \\____\\/    |  ");
-    println!("{}{}{}{}{}{}", color::Fg(color_main), "|        | |   | |        |", color::Fg(color_leaves), "|           | /", color::Fg(color_main), "|     | | |     || |     | |     || |     /____/| | ||| |    |____/|  ");
-    println!("{}{}{}{}{}{}", color::Fg(color_main), "|________|/     \\|________|", color::Fg(color_leaves), "|___________|/ ", color::Fg(color_main), "|_____|/ \\|_____| \\|_____|\\|_____| \\|_____|     |\\|_|/ \\|____|   | |  ");
-    println!("{}{}{}{}{}{}", color::Fg(color_main), "                           ", color::Fg(color_leaves), "               ", color::Fg(color_main), "                                          |____/             |___|/   ");
-    println!("{}                                                                                                     v{}.{}.{}", color::Fg(color::Reset), env!("CARGO_PKG_VERSION_MAJOR"), env!("CARGO_PKG_VERSION_MINOR"), env!("CARGO_PKG_VERSION_PATCH"));
-}
+use piston_window::*;
 
 fn main() {
-    let _guard = termion::init();
-    prep_terminal();
-    show_title();
+    let mut window: PistonWindow = WindowSettings::new(
+            format!("Mirage v{}.{}.{}",
+                    env!("CARGO_PKG_VERSION_MAJOR"),
+                    env!("CARGO_PKG_VERSION_MINOR"),
+                    env!("CARGO_PKG_VERSION_PATCH")),
+            [1280, 720]
+        )
+        .exit_on_esc(true)
+        //.opengl(OpenGL::V2_1) // Set a different OpenGl version
+        .build()
+        .unwrap();
+
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder("assets").unwrap();
+    println!("{:?}", assets);
+    let ref font = assets.join("FiraSans-Regular.ttf");
+    let factory = window.factory.clone();
+    let mut glyphs = Glyphs::new(font, factory, TextureSettings::new()).unwrap();
+
+    window.set_lazy(true);
+    let mut titlex = 0.0;
+    let mut titley = 0.0;
+    let mut keys = [false, false, false, false];
+    let mut events = Events::new(EventSettings::new().lazy(true));
+    while let Some(e) = events.next(&mut window) {
+        window.draw_2d(&e, |c, g| {
+            let transform = c.transform.scale(4.0, 4.0).trans(0.0, 24.0).trans(titlex, titley);
+
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32).draw(
+                "   I",
+                &mut glyphs,
+                &c.draw_state,
+                transform, g
+            ).unwrap();
+            text::Text::new_color([0.6, 0.6, 1.0, 1.0], 32).draw(
+                "M RAGE",
+                &mut glyphs,
+                &c.draw_state,
+                transform, g
+            ).unwrap();
+        });
+
+        match e.press_args() {
+            Some(Button::Keyboard(Key::D)) => { println!("Pressed D"); keys[0] = true },
+            Some(Button::Keyboard(Key::A)) => keys[1] = true,
+            Some(Button::Keyboard(Key::S)) => keys[2] = true,
+            Some(Button::Keyboard(Key::W)) => keys[3] = true,
+            _ => (),
+        }
+        match e.release_args() {
+            Some(Button::Keyboard(Key::D)) => { println!("Released D"); keys[0] = false },
+            Some(Button::Keyboard(Key::A)) => keys[1] = false,
+            Some(Button::Keyboard(Key::S)) => keys[2] = false,
+            Some(Button::Keyboard(Key::W)) => keys[3] = false,
+            _ => (),
+        }
+        if keys[0] {
+            titlex += 1.0;
+        }
+        if keys[1] {
+            titlex -= 1.0;
+        }
+        if keys[2] {
+            titley += 1.0;
+        }
+        if keys[3] {
+            titley -= 1.0;
+        }
+    }
 }
